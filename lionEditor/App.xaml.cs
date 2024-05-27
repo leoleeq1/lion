1500679a1afe51d41f5ca19d40893db2853e9532
Leo Lee <leoleeidc@gmail.com>
2024-05-28 02:51:36 +0900

    Project Setup

---
+++ lionEditor/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace lionEditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
